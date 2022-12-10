c433c850746cb1ad75467e89b2176e0cdead689b
Papageorgiadis Savvas <papsavas56@gmail.com>
2022-12-10 20:15:24 +0200

    fix: remove `BackNav` component

--- src/Components/BackNav.tsx
+++
@@ -1,9 +0,0 @@
-export default function BackNav() {
-  const router = useRouter();
-  const navigate = () => router.back();
-  return (
-    <div style={{ cursor: "pointer" }} onClick={navigate}>
-      Back
-    </div>
-  );
-}
