c22cfbbe5b6ad40b7e29a8c16b01b8377ed0cf03
Papageorgiadis Savvas <papsavas56@gmail.com>
2022-12-10 20:17:17 +0200

    fix(ThemeToggle): switch styles

--- src/Components/ThemeToggle.tsx
+++ src/Components/ThemeToggle.tsx
@@ -9,16 +9,16 @@ const ThemeToggle: FC<{}> = () => {
       <div
         className={`flex w-16 h-6 
         ${onTheme(
-          "bg-gradient-to-l from-slate-700 to-slate-500",
-          "bg-gradient-to-r from-white to-yellow-100"
+          "bg-gradient-to-r from-white to-yellow-100",
+          "bg-gradient-to-l from-slate-700 to-slate-500"
         )} rounded-full transition-all duration-500`}
         onClick={toggleTheme}
       >
         <span
           className={`flex self-center h-8 w-8 
           ${onTheme(
-            "bg-gray-200 ml-8 outline-3 outline-gray-100",
-            "bg-yellow-200 outline-dotted outline-yellow-200"
+            "bg-yellow-200 outline-dotted outline-yellow-200",
+            "bg-gray-200 ml-8 outline-3 outline-gray-100"
           )} rounded-full shadow-lg  outline-2 transition-all duration-500`}
         />
       </div>
