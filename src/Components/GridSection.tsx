51f75ec12aaff41dcf976aa72ad98bd1312563b7
Papageorgiadis Savvas <papsavas56@gmail.com>
2022-12-12 20:53:37 +0200

    fix: delay on theme change, shadow size

--- src/Components/GridSection.tsx
+++ src/Components/GridSection.tsx
@@ -14,12 +14,15 @@ const GridSection: FC<{ title: string; description: string }> = ({
        "text-slate-600 bg-gray-50 border-gray-100 hover:bg-gray-100",
        "text-slate-200 bg-slate-700 border-gray-600 hover:bg-slate-600"
      )} 
-    group hover:shadow-xl hover:scale-110 transition-all duration-300`}
+    group hover:shadow-lg hover:scale-110 transition-transform duration-300`}
     >
       <div className="flex flex-col items-center break-all">
         <h2 className="text-5xl mb-5">{title}</h2>
         {/* // TODO: space evenly */}
-        <p className="place-self-center max-h-24 overflow-auto [&::-webkit-scrollbar]:hidden opacity-0 group-hover:opacity-100 transition-all duration-500">
+        <p
+          className="place-self-center max-h-24 overflow-auto [&::-webkit-scrollbar]:hidden
+         opacity-0 group-hover:opacity-100 transition-opacity duration-700"
+        >
           {description}
         </p>
       </div>
