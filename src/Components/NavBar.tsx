6dfa4dfd77ba7d22c32d782a4b7e913e8e5e0725
Papageorgiadis Savvas <papsavas56@gmail.com>
2022-12-13 02:38:50 +0200

    chore: remove navbar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -4,25 +4,17 @@ import "../styles/globals.css";
 import ThemeToggle from "./ThemeToggle";
 
 const NavBar: FC<{}> = () => {
-  const routes = ["home", "music", "code"] as const;
   const { onTheme } = useTheme();
 
   return (
-    <div className={`relative ${onTheme(`bg-[#424549]`, `bg-gray-600`)} mb-12`}>
-      <div className="flex flex-row overflow-x-auto justify-center items-center gap-[8%]">
-        {routes.map((r, i) => (
-          <a
-            href={r === "home" ? `/` : `/${r}`} // "/" +
-            className={`text-white text-2xl px-6 py-2 m-1 capitalize rounded-2xl hover:bg-gray-800 hover:shadow-lg`}
-            key={i}
-          >
-            {r}
-          </a>
-        ))}
-        {/* 
-          //TODO!: On top when resizing. Make it same level but not affecting route positioning and spacing 
-        */}
-        <div className="absolute right-0 mr-28">
+    <div className={`min-w-full pb-12`}>
+      <div
+        className={`absolute right-0 mt-4 mr-[8%] py-4 px-3 rounded-2xl ${onTheme(
+          `bg-[#424549]`,
+          `bg-gray-600`
+        )} hover:shadow-md hover:scale-95 transition-all duration-300`}
+      >
+        <div className="">
           <ThemeToggle />
         </div>
       </div>
