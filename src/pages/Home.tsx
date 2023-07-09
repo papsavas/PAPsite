2b026592945eb78991d524bcb4ea6a4d8a457201
Papageorgiadis Savvas <papsavas56@gmail.com>
2023-07-09 19:34:57 +0300

    revert: hide grids

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,4 @@
-import GridSection from "../Components/GridSection";
 import MediaRedirect from "../Components/MediaRedirect";
-import { gridSectionData } from "../values/gridSection.json";
 import { socialMedia } from "../values/socialMedia.json";
 
 const Home = () => {
@@ -10,37 +8,33 @@ const Home = () => {
         <img
           src="/IMG_2172PH4.jpg"
           className="h-48 w-48 object-fill rounded-full pointer-events-none"
-          alt="pic"
-        ></img>
+          alt="pic"></img>
       </div>
       <div className="flex justify-center">
         <h1
           className={`font-extrabold text-7xl text-transparent bg-clip-text bg-gradient-to-tr
           from-gray-700 to-gray-400 dark:from-gray-600 dark:to-gray-400
-           `}
-        >
+           `}>
           paqsavaƨ
         </h1>
       </div>
       <div
-        className={`flex justify-center text-3xl m-5 text-text dark:text-text-dark`}
-      >
+        className={`flex justify-center text-3xl m-5 text-text dark:text-text-dark`}>
         <p>Savvas Papageorgiadis</p>
       </div>
-      <div
+      {/* <div
         className={`grid grid-flow-row lg:grid-flow-col lg:grid-cols-2 place-items-center gap-[7%] mx-[10%] my-28`}
       >
         {gridSectionData.map((data) => (
           <GridSection {...data} />
         ))}
-      </div>
+      </div> */}
       <div className={`flex flex-col justify-center items-center pt-10 pb-20 `}>
         <div
           className={`flex flex-col space-y-4 py-4 px-1 shadow-md border-2  rounded-3xl
               bg-grid border-border
               dark:bg-grid-dark dark:border-border-dark
-            `}
-        >
+            `}>
           {socialMedia.map((sm) => (
             <MediaRedirect {...sm} />
           ))}
@@ -52,8 +46,7 @@ const Home = () => {
           <a
             href="https://icons8.com"
             target="_blank"
-            className="underline underline-offset-2 text-slate-400"
-          >
+            className="underline underline-offset-2 text-slate-400">
             icons8
           </a>
         </div>
